fe83435ad02ad729c74ba00bb1d35b21717cc44e
Alexandru Barbur <root.ctrlc@gmail.com>
2017-07-22 00:37:25 -0400

    Modify the Device class to encapsulate attributes and actions.

--- pkg/bridge-hue/mdcs_bridge_hue/generic/device.py
+++ pkg/bridge-hue/mdcs_bridge_hue/generic/device.py
@@ -16,3 +16,31 @@ class Device:
 
         # create a unique identifier
         self.uuid = uuid.uuid4()
+
+        # device state and actions
+        self.attributes = {}
+        self.actions = {}
+
+    def add_attribute(self, attribute):
+        if attribute.path in self.attributes or attribute.path in self.actions:
+            raise KeyError("attribute path is not unique")
+
+        self.attributes[attribute.path] = attribute
+
+    def remove_attribute(self, attribute):
+        if attribute.path not in self.attributes:
+            raise KeyError("attribute not found")
+
+        del self.attributes[attribute.path]
+
+    def add_action(self, action):
+        if action.path in self.attributes or action.path in self.actions:
+            raise KeyError("action path is not unique")
+
+        self.actions[action.path] = action
+
+    def remove_action(self, action):
+        if action.path not in self.actions:
+            raise KeyError("action not found")
+
+        self.actions[action.path] = action

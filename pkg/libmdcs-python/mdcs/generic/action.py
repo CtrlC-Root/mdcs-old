a8e8ed53f2b92e8b0094f4c6823df7a48f0e52ed
Alexandru Barbur <root.ctrlc@gmail.com>
2017-07-26 20:08:17 -0400

    Modify the Action class to store an input and output schema. Add a DelegatedAction class.

--- pkg/libmdcs-python/mdcs/generic/action.py
+++ pkg/libmdcs-python/mdcs/generic/action.py
@@ -6,12 +6,27 @@ class Action:
     An action that changes a device's current state.
     """
 
-    def __init__(self, path):
+    def __init__(self, path, input_schema, output_schema):
         self.path = path
+        self.input_schema = input_schema
+        self.output_schema = output_schema
 
-    def run(self):
+    def run(self, input_data):
         """
         Run the action with the given arguments.
         """
 
         raise NotImplemented()
+
+
+class DelegatedAction(Action):
+    """
+    An action that uses an external function to modify the device's current state.
+    """
+
+    def __init__(self, path, input_schema, output_schema, handler):
+        super().__init__(path, input_schema, output_schema)
+        self.handler = handler
+
+    def run(self, input_data):
+        return self.handler()

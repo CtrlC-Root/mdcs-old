98589e77b10f824a5f6f18df2a07a8df808fbc5a
Alexandru Barbur <root.ctrlc@gmail.com>
2017-07-28 00:32:04 -0400

    Replace the default application with a router view. Add initial Dashboard component.

---
+++ pkg/mdcs-web/src/Dashboard.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="container">
+    <h3 class="display-3 text-xs-center">Dashboard</h3>
+
+    <!-- Controls -->
+    <div class="row">
+      <!-- Control Card -->
+      <div class="col-sm-12 col-md-6 col-lg-4">
+        <div class="card">
+          <div class="card-block">
+            <h4 class="card-title">hue-light-1: brightness</h4>
+            <p class="card-text">TODO</p>
+            <a href="#" class="btn btn-danger">X</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'dashboard',
+
+  data () {
+    return {}
+  },
+
+  methods: {}
+}
+</script>
+
+<style>
+</style>
--- pkg/mdcs-web/src/main.js
+++ pkg/mdcs-web/src/main.js
@@ -1,7 +1,33 @@
 import Vue from 'vue'
-import App from './App.vue'
+import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
 
-new Vue({
-  el: '#app',
-  render: h => h(App)
+import Dashboard from './Dashboard.vue'
+/*
+import ContentList from './ContentList.vue'
+import ContentDetail from './ContentDetail.vue'
+import TagList from './TagList.vue'
+import TagDetail from './TagDetail.vue'
+*/
+
+// configure Vue plugins
+Vue.use(VueRouter)
+Vue.use(VueResource)
+
+// configure Vue default settings
+Vue.http.options.root = 'http://127.0.0.1:8000'
+
+// create the Vue router
+const router = new VueRouter({
+  routes: [
+    {name: 'dashboard', path: '/', component: Dashboard}
+  ]
 })
+
+// create the Vue application
+const app = new Vue({
+  router: router,
+  http: {
+    root: 'http://127.0.0.1:8000/'
+  }
+}).$mount("#app")

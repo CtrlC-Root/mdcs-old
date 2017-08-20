280aa806fe124d67b0e7cdaed3d3d35068f8ecba
Alexandru Barbur <root.ctrlc@gmail.com>
2017-08-20 18:17:19 -0400

    Simplify the Response object with a shortcut using HTTPStatus codes.

--- pkg/libmdcs-python/mdcs/http/response.py
+++ pkg/libmdcs-python/mdcs/http/response.py
@@ -2,7 +2,12 @@ from http import HTTPStatus
 
 
 class Response:
-    def __init__(self, headers={}, status_code=HTTPStatus.OK, content=None):
-        self.headers = headers
+    def __init__(self, status_code=HTTPStatus.OK, headers={}, content=None):
         self.status_code = status_code
+        self.headers = headers
         self.content = content
+
+        # shortcut for creating a generic response from an HTTP status code
+        if not self.content:
+            self.headers['Content-Type'] = 'text/plain'
+            self.content = self.status_code.phrase

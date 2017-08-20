2432fb22762fe8f6ec7c6acf30547e5f8672d4b1
Alexandru Barbur <root.ctrlc@gmail.com>
2017-08-19 21:06:10 -0400

    Remove the unnecessary Dashboard components from the web interface and make the Network component the default top-level view.

--- pkg/mdcs-web/src/main.js
+++ pkg/mdcs-web/src/main.js
@@ -4,7 +4,6 @@ import VueRouter from 'vue-router';
 import VueResource from 'vue-resource';
 
 import store from './store/index';
-import Dashboard from './components/Dashboard.vue';
 import Network from './components/Network.vue';
 import NodeDetail from './components/node/NodeDetail.vue';
 import DeviceDetail from './components/device/DeviceDetail.vue';
@@ -17,8 +16,7 @@ Vue.use(VueResource);
 // create the Vue router
 const router = new VueRouter({
   routes: [
-    {name: 'dashboard', path: '/', component: Dashboard},
-    {name: 'network', path: '/network', component: Network},
+    {name: 'network', path: '/', component: Network},
     {name: 'node-detail', path: '/node/:node', component: NodeDetail},
     {name: 'device-detail', path: '/node/:node/device/:device', component: DeviceDetail}
   ]

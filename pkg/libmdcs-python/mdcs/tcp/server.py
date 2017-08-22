05fe2909cf395e900c67a857210dc80f01a907e2
Alexandru Barbur <root.ctrlc@gmail.com>
2017-08-21 21:59:18 -0400

    Add initial TCP API server logic for handling different protocol messages.

--- pkg/libmdcs-python/mdcs/tcp/server.py
+++ pkg/libmdcs-python/mdcs/tcp/server.py
@@ -1,17 +1,98 @@
+import time
+from io import BytesIO
 from socketserver import TCPServer, BaseRequestHandler
+
+from avro.ipc import Responder, FramedReader, FramedWriter
+from avro.schema import AvroException
+
+from .schema import API_PROTOCOL
+
+
+class NodeResponder(Responder):
+    def __init__(self, node):
+        super().__init__(API_PROTOCOL)
+        self.node = node
+
+    def Invoke(self, message, request):
+        if message.name == 'read' or message.name == 'write':
+            target = request['target']
+
+            # retrieve the device
+            if target['device'] not in self.node.devices:
+                return {'message': 'device not found'}
+
+            device = self.node.devices[target['device']]
+
+            # retrieve the attribute
+            if target['attribute'] not in device.attributes:
+                return {'message': 'attribute not found'}
+
+            attribute = device.attributes[target['attribute']]
+
+            # check if this is a read
+            if message.name == 'read':
+                # check if we can read the attribute
+                if not attribute.readable:
+                    return {
+                        'message': 'attribute is not readable',
+                        'device': device.name,
+                        'attribute': attribute.path
+                    }
+
+                # TODO: read the value
+                return {'value': bytes([0xDE, 0xAD, 0xBE, 0xEF]), 'time': int(round(time.time() * 1000))}
+
+            # check if we can write to the attribute
+            if not attribute.writable:
+                return {
+                    'message': 'attribute is not writable',
+                    'device': device.name,
+                    'attribute': attribute.path
+                }
+
+            # XXX retrieve the new value
+            data = request['data']
+            value = data['value']
+
+            # TODO: write the value
+            return {'value': bytes([0xDE, 0xAD, 0xBE, 0xEF]), 'time': int(round(time.time() * 1000))}
+
+        elif message.name == 'run':
+            # TODO: run the action
+            return {'message': 'running actions is not implemented'}
+
+        else:
+            # unknown message
+            raise AvroException("unexpected message: ", msg.name)
 
 
 class NodeTCPRequestHandler(BaseRequestHandler):
-    @property
-    def node(self):
-        return self.server.node
-
     def handle(self):
         """
         Process requests from a TCP API client.
         """
 
-        pass
+        # TODO: modify this to handle multiple consecutive requests instead of one per connection
+
+        # receive the request data
+        recv_data = self.request.recv(65535) # XXX lol what
+        recv_buffer = BytesIO(recv_data)
+
+        # read the request data
+        reader = FramedReader(recv_buffer)
+        request_data = reader.Read()
+
+        # process the request
+        responder = NodeResponder(self.server.node)
+        response_data = responder.Respond(request_data)
+
+        # write the response data
+        send_buffer = BytesIO()
+        writer = FramedWriter(send_buffer)
+        writer.Write(response_data)
+
+        # send the response data
+        self.request.send(send_buffer.getvalue())
 
 
 class NodeTCPServer(TCPServer):

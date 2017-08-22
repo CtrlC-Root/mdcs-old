148d353861ad45452181238078066b73fbcad03e
Alexandru Barbur <root.ctrlc@gmail.com>
2017-08-21 21:59:30 -0400

    Update the console client to use the new RPC interface.

--- pkg/mdcs-console/mdcs_console/ctl.py
+++ pkg/mdcs-console/mdcs_console/ctl.py
@@ -2,15 +2,17 @@
 
 import io
 import json
+import time
 import socket
 import argparse
 
 import avro.io
+import avro.ipc
 import avro.schema
 import avro.datafile
 import pkg_resources
 
-from mdcs.tcp.schema import REQUEST_SCHEMA, RESPONSE_SCHEMA
+from mdcs.tcp import API_PROTOCOL, TCPTransceiver
 
 
 def main():
@@ -26,10 +28,6 @@ def main():
 
     args = parser.parse_args()
 
-    # TODO: connect to the server
-    tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    tcp_socket.connect((args.host, args.tcp_port))
-
     # XXX serialize attribute value using hard-coded schema
     # XXX ideally we would get this using the HTTP API from the server
     value_schema = avro.schema.Parse(json.dumps({'type': 'boolean'}))
@@ -38,46 +36,23 @@ def main():
     value_writer.append(bool(input('on: ').strip().lower() in ['yes', 'true']))
 
     value_writer.flush()
-    value_buffer.seek(0)
-    value_data = value_buffer.read()
+    value_data = value_buffer.getvalue()
 
-    # TODO: create the request
-    request = {'command': {'device': 'hue-group-4', 'attribute': 'on', 'value': value_data}}
+    # XXX
+    client = TCPTransceiver(args.host, args.tcp_port)
+    requestor = avro.ipc.Requestor(API_PROTOCOL, client)
 
-    # TODO: serialize the request
-    request_buffer = io.BytesIO()
-    writer = avro.datafile.DataFileWriter(request_buffer, avro.io.DatumWriter(), REQUEST_SCHEMA)
-    writer.append(request)
-    writer.flush()
+    # XXX
+    response = requestor.Request('write', {
+        'target': {
+            'device': 'hue-group-0',
+            'attribute': 'on'
+        },
+        'data': {
+            'value': value_buffer.getvalue(),
+            'time': int(round(time.time() * 1000))
+        }
+    })
 
-    # TODO: send the request
-    request_buffer.seek(0)
-    request_data = request_buffer.read()
-    tcp_socket.send(request_data)
-
-    # TODO: read the response
-    response_data = tcp_socket.recv(10240)
-    response_buffer = io.BytesIO(response_data)
-    reader = avro.datafile.DataFileReader(response_buffer, avro.io.DatumReader())
-
-    # TODO: process the response
-    for response in reader:
-        result = response['result']
-        if 'message' in result:
-            print("error: {0}".format(result))
-
-        elif 'value' in result:
-            when = result['when']
-            data = result['value']
-
-            data_reader = avro.datafile.DataFileReader(io.BytesIO(data), avro.io.DatumReader())
-            for value in data_reader:
-                print("value {0} @ {1}".format(value, when))
-
-            data_reader.close()
-
-        else:
-            print("confirm @ {0}".format(result['when']))
-
-    # TODO: close the reader
-    reader.close()
+    # XXX
+    print(response)

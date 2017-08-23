31ac51cf2aa2eb2f81b0b53329ed347572a28dc4
Alexandru Barbur <root.ctrlc@gmail.com>
2017-08-23 00:33:23 -0400

    Implement reading and writing attribute values in the console client.

--- pkg/mdcs-console/mdcs_console/ctl.py
+++ pkg/mdcs-console/mdcs_console/ctl.py
@@ -1,14 +1,77 @@
 #!/usr/bin/env python
 
 import json
-import time
 import argparse
+from http import HTTPStatus
+from datetime import datetime
 
+import requests
 import avro.ipc
 import avro.schema
 
 from mdcs.tcp import API_PROTOCOL, TCPTransceiver
 from mdcs.tcp.avro import serialize_value, unserialize_value
+
+
+def read_attribute(args):
+    """
+    Read an attribute value.
+    """
+
+    # retrieve the attribute schema
+    response = requests.get("http://{host}:{http_port}/d/{device}/at/{attribute}".format(**vars(args)))
+    if response.status_code != HTTPStatus.OK:
+        print("error retrieving config from bridge: {0}".format(response))
+        sys.exit(1)
+
+    attribute = response.json()
+    schema = avro.schema.Parse(json.dumps(attribute['schema']))
+
+    # create the Avro IPC client
+    client = TCPTransceiver(args.host, args.tcp_port)
+    requestor = avro.ipc.Requestor(API_PROTOCOL, client)
+
+    # read the attribute value
+    response = requestor.Request('read', {'target': {'device': args.device, 'attribute': args.attribute}})
+    value = unserialize_value(schema, response['value'])
+    time = datetime.fromtimestamp(response['time'] / 1000.0)
+
+    # display the value
+    print("Time: {0}\nValue: {1}".format(time, value))
+
+
+def write_attribute(args):
+    """
+    Write an attribute value.
+    """
+
+    # retrieve the attribute schema
+    response = requests.get("http://{host}:{http_port}/d/{device}/at/{attribute}".format(**vars(args)))
+    if response.status_code != HTTPStatus.OK:
+        print("error retrieving config from bridge: {0}".format(response))
+        sys.exit(1)
+
+    attribute = response.json()
+    schema = avro.schema.Parse(json.dumps(attribute['schema']))
+
+    # create the Avro IPC client
+    client = TCPTransceiver(args.host, args.tcp_port)
+    requestor = avro.ipc.Requestor(API_PROTOCOL, client)
+
+    # write the attribute value
+    response = requestor.Request('write', {
+        'target': {'device': args.device, 'attribute': args.attribute},
+        'data': {
+            'value': serialize_value(schema, json.loads(args.value)),
+            'time': int(datetime.now().timestamp() * 1000)
+        }
+    })
+
+    value = unserialize_value(schema, response['value'])
+    time = datetime.fromtimestamp(response['time'] / 1000.0)
+
+    # display the value
+    print("Time: {0}\nValue: {1}".format(time, value))
 
 
 def main():
@@ -21,29 +84,20 @@ def main():
     parser.add_argument('--host', type=str, default='127.0.0.1', help="node hostname or IP address")
     parser.add_argument('--http-port', type=int, default=5510, help="HTTP API port")
     parser.add_argument('--tcp-port', type=int, default=5511, help="TCP API port")
+    subparsers = parser.add_subparsers()
+
+    parser_read = subparsers.add_parser('read')
+    parser_read.set_defaults(handler=read_attribute)
+    parser_read.add_argument('device', type=str, help="device name")
+    parser_read.add_argument('attribute', type=str, help="attribute name")
+
+    parser_write = subparsers.add_parser('write')
+    parser_write.set_defaults(handler=write_attribute)
+    parser_write.add_argument('device', type=str, help="device name")
+    parser_write.add_argument('attribute', type=str, help="attribute name")
+    parser_write.add_argument('value', type=str, help="JSON encoded value")
 
     args = parser.parse_args()
 
-    # XXX serialize attribute value using hard-coded schema
-    # XXX ideally we would get this using the HTTP API from the server
-    schema = avro.schema.Parse(json.dumps({'type': 'boolean'}))
-    value = bool(input('on: ').strip().lower() in ['yes', 'true'])
-
-    # XXX
-    client = TCPTransceiver(args.host, args.tcp_port)
-    requestor = avro.ipc.Requestor(API_PROTOCOL, client)
-
-    # XXX
-    response = requestor.Request('write', {
-        'target': {
-            'device': 'hue-group-0',
-            'attribute': 'on'
-        },
-        'data': {
-            'value': serialize_value(schema, value),
-            'time': int(round(time.time() * 1000))
-        }
-    })
-
-    # XXX
-    print(response)
+    # run the handler
+    args.handler(args)

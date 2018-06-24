f683b16ac89c77924f29caffa3877cd8636a5dcc
Alexandru Barbur <root.ctrlc@gmail.com>
2018-06-23 20:54:55 -0400

    Add initial SQLALchemy integration.

--- pkg/reactor/mdcs_reactor/application.py
+++ pkg/reactor/mdcs_reactor/application.py
@@ -1,8 +1,32 @@
-from flask import Flask
+from flask import Flask, g
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
 
 
 # create the application
 application = Flask('mdcs_reactor', instance_relative_config=True)
+application.config['DATABASE_URI'] = 'sqlite:///reactor.db'
+
+
+# create the database engine and session factory
+# http://docs.sqlalchemy.org/en/latest/orm/tutorial.html#creating-a-session
+database_engine = create_engine(application.config['DATABASE_URI'], convert_unicode=True)
+database_session_factory = sessionmaker(bind=database_engine)
+
+
+# create and finalize a database session
+# http://flask.pocoo.org/docs/1.0/patterns/sqlite3/
+# http://flask.pocoo.org/docs/1.0/patterns/sqlalchemy/
+@application.before_first_request
+def create_db_session():
+    g.db = database_session_factory()
+
+
+@application.teardown_appcontext
+def finalize_db_session(exception=None):
+    db = getattr(g, 'db', None)
+    if db is not None:
+        db.close()
 
 
 # register top-level utility views
@@ -26,4 +50,6 @@ def health():
 
 
 # import views after the application object exists to avoid a circular import error
-from .views import *
+# disable the pycodestyle E402 warning for this one line since we know what we're doing
+# https://pycodestyle.readthedocs.io/en/latest/intro.html#error-codes
+from .views import *  # noqa

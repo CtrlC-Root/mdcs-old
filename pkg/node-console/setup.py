cce13a7e9a8584009103898147c31e1da8f1f9b0
Alexandru Barbur <root.ctrlc@gmail.com>
2018-06-30 23:23:56 -0400

    Add requests and beautifultable reqs to node-console pkg.

--- pkg/node-console/setup.py
+++ pkg/node-console/setup.py
@@ -12,7 +12,9 @@ setup(
 
     packages=find_packages(),
     install_requires=[
-        'mdcs >= 0.1'
+        'mdcs >= 0.1',
+        'requests >= 2.18.0',
+        'beautifultable >= 0.5.0'
     ],
 
     entry_points={

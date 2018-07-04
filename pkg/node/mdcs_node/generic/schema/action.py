e65ab126b63a77ce2c7ea788867a4a317b504532
Alexandru Barbur <root.ctrlc@gmail.com>
2018-07-03 21:19:09 -0400

    Properly serialize Action and Attribute Avro schemas to and from JSON.

--- pkg/node/mdcs_node/generic/schema/action.py
+++ pkg/node/mdcs_node/generic/schema/action.py
@@ -1,3 +1,5 @@
+import json
+
 from marshmallow import Schema, post_load, pre_dump
 from marshmallow.fields import String
 from marshmallow.validate import Length
@@ -11,19 +13,19 @@ class ActionSchema(Schema):
     """
 
     path = String(validate=Length(min=1))
-    input_schema = String(validate=Length(min=1))
-    output_schema = String(validate=Length(min=1))
+    input_schema = String(validate=Length(min=1))  # XXX: JSON field instead?
+    output_schema = String(validate=Length(min=1))  # XXX: JSON field instead?
 
     @post_load
     def create_action(self, data):
         return Action(
             path=data['path'],
-            input_schema=data['input_schema'],
-            output_schema=data['output_schema'])
+            input_schema=json.loads(data['input_schema']),
+            output_schema=json.loads(data['output_schema']))
 
     @pre_dump
     def jsonify_action(self, action):
         return {
             'path': action.path,
-            'input_schema': action.input_schema.to_json(),
-            'output_schema': action.output_schema.to_json()}
+            'input_schema': json.dumps(action.input_schema.to_json()),
+            'output_schema': json.dumps(action.output_schema.to_json())}
--- pkg/node/mdcs_node/generic/schema/attribute.py
+++ pkg/node/mdcs_node/generic/schema/attribute.py
@@ -1,3 +1,5 @@
+import json
+
 from marshmallow import Schema, post_load, pre_dump
 from marshmallow.fields import String, List, Dict
 from marshmallow.validate import Length, ContainsOnly
@@ -13,18 +15,18 @@ class AttributeSchema(Schema):
 
     path = String(validate=Length(min=1))
     flags = List(String(), validate=ContainsOnly(choices=[flag.name for flag in AttributeFlags]))
-    schema = String(validate=Length(min=1))
+    schema = String(validate=Length(min=1))  # XXX: JSON field instead?
 
     @post_load
     def create_attribute(self, data):
         return Attribute(
             path=data['path'],
             flags=[flag for flag in AttributeFlags if flag.name in data['flags']],
-            schema=data['schema'])
+            schema=json.loads(data['schema']))
 
     @pre_dump
     def jsonify_attribute(self, attribute):
         return {
             'path': attribute.path,
             'flags': [flag.name for flag in AttributeFlags if flag in attribute.flags],
-            'schema': attribute.schema.to_json()}
+            'schema': json.dumps(attribute.schema.to_json())}

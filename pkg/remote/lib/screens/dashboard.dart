48d17a71dd3ad1f6466c3a127302edba281f8913
Alexandru Barbur <root.ctrlc@gmail.com>
2018-07-14 12:34:28 -0400

    Use the floating action button in the Scaffold widget for the primary action instead of a row at the bottom of the screen.

--- pkg/remote/lib/screens/dashboard.dart
+++ pkg/remote/lib/screens/dashboard.dart
@@ -7,11 +7,26 @@ class DashboardScreen extends StatelessWidget {
 
   DashboardScreen({Key key, @required this.repository}) : super(key: key);
 
+  void onNavigateSettings() {
+    // TODO: do something
+  }
+
+  void onNewAction() {
+    // TODO: do something
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Remote Control'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.settings),
+            tooltip: "Settings",
+            onPressed: this.onNavigateSettings,
+          ),
+        ],
       ),
       body: Column(
         children: [
@@ -19,31 +34,16 @@ class DashboardScreen extends StatelessWidget {
           Expanded(
             child: ActionList(repository: repository),
           ),
-          DashboardControls(),
         ],
       ),
-    );
-  }
-}
-
-class DashboardControls extends StatelessWidget {
-  void onNewAction() {
-    // TODO: do something
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.end,
-      children: [
-        IconButton(
-          icon: Icon(Icons.add_circle),
-          iconSize: 48.0,
-          color: Color.fromRGBO(0, 0, 196, 1.0),
-          tooltip: "New",
-          onPressed: this.onNewAction,
-        ),
-      ],
+      floatingActionButton: IconButton(
+        icon: Icon(Icons.add_circle),
+        iconSize: 48.0,
+        color: Color.fromRGBO(0, 0, 196, 1.0),
+        tooltip: "New",
+        onPressed: this.onNewAction,
+      ),
+      // floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
     );
   }
 }

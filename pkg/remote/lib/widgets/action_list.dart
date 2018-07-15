e9e0be489b640bc96e70bc74ef396eee71c6cb05
Alexandru Barbur <root.ctrlc@gmail.com>
2018-07-14 22:34:24 -0400

    Refactor the ActionList Card related code into a separate ActionCard widget.

--- pkg/remote/lib/widgets/action_list.dart
+++ pkg/remote/lib/widgets/action_list.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:remote/repository.dart';
 import 'package:remote/models/action.dart';
+import 'package:remote/widgets/action_card.dart';
 
 class ActionList extends StatefulWidget {
   final Repository repository;
@@ -40,45 +41,13 @@ class _ActionListState extends State<ActionList> {
 
   @override
   Widget build(BuildContext context) {
-    List<Action> actions = this.widget.repository.actions.values;
+    final List<Action> actions = this.widget.repository.actions.values;
     return ListView.builder(
       itemCount: actions.length,
       itemBuilder: (context, index) {
-        Action action = actions[index];
-        return Card(
-          child: Container(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
-              children: [
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        action.title,
-                        style: DefaultTextStyle.of(context).style.apply(fontSizeFactor: 1.25),
-                      ),
-                      Text(
-                        action.description,
-                        style: DefaultTextStyle.of(context).style.copyWith(fontWeight: FontWeight.w300),
-                      ),
-                    ],
-                  ),
-                ),
-                IconButton(
-                  icon: Icon(Icons.play_arrow),
-                  tooltip: "Run",
-                  onPressed: () {},
-                ),
-                IconButton(
-                  icon: Icon(Icons.details),
-                  color: Color.fromRGBO(0, 196, 0, 1.0),
-                  tooltip: "Details",
-                  onPressed: () {},
-                ),
-              ],
-            )
-          ),
+        return ActionCard(
+          repository: this.widget.repository,
+          actionUuid: actions[index].uuid
         );
       },
     );

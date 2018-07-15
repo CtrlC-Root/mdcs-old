fe1dca9f079254f0576a8c186fadd326edd95fb6
Alexandru Barbur <root.ctrlc@gmail.com>
2018-07-14 21:35:03 -0400

    Update the ActionStore to add the description field.

--- pkg/remote/lib/stores/action.dart
+++ pkg/remote/lib/stores/action.dart
@@ -5,10 +5,11 @@ import 'package:remote/models/action.dart';
 class ActionNotifier extends ModelNotifier<Action> {
   ActionNotifier(Action initialValue) : super(initialValue);
 
-  void update({String uuid, String title, String content}) {
+  void update({String uuid, String title, String description, String content}) {
     this.value = Action(
       uuid: uuid ?? this.value.uuid,
       title: title ?? this.value.title,
+      description: description ?? this.value.description,
       content: content ?? this.value.content
     );
   }

1eb4bae3bb571c4626379ee58fe30efa725f3723
Alexandru Barbur <root.ctrlc@gmail.com>
2018-07-14 21:34:06 -0400

    Add a description field to the Action model in the Dart app. Write code to parse an Action from JSON.

--- pkg/remote/lib/models/action.dart
+++ pkg/remote/lib/models/action.dart
@@ -4,7 +4,17 @@ import 'package:remote/models/model.dart';
 class Action extends Model {
     String uuid;
     String title;
+    String description;
     String content;
 
-    Action({this.uuid, this.title, this.content});
+    Action({this.uuid, this.title, this.description, this.content});
+
+    static Action fromJSON(Map<String, dynamic> data) {
+      return Action(
+        uuid: data['uuid'] as String,
+        title: data['title'] as String,
+        description: data['description'] as String,
+        content: data['content'] as String,
+      );
+    }
 }

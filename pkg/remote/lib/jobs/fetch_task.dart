678a0c756178333a281592d65b1ec103f7a9abd5
Alexandru Barbur <root.ctrlc@gmail.com>
2018-07-15 20:50:39 -0400

    Add a job to fetch a task by it's primary key.

---
+++ pkg/remote/lib/jobs/fetch_task.dart
@@ -0,0 +1,41 @@
+import 'dart:async';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:remote/models/models.dart';
+import 'package:remote/jobs/job.dart';
+
+class FetchTaskJob extends Job {
+  final Uri _api;
+  final String _primaryKey;
+  Task _task;
+
+  FetchTaskJob(Uri api, String primaryKey) : this._api = api, this._primaryKey = primaryKey, this._task = null, super();
+
+  @override
+  Future run() async {
+    final client = http.Client();
+
+    // retrieve task
+    final taskUri = this._api.replace(path: '/task/${this._primaryKey}');
+    final taskResponse = await client.get(taskUri.toString());
+
+    if (taskResponse.statusCode != 200) {
+      this.fail(Exception('failed to retrieve task: ${this._primaryKey}'));
+      return;
+    }
+
+    // parse task
+    final Map<String, dynamic> taskData = json.decode(taskResponse.body);
+    this._task = Task.fromJSON(taskData);
+
+    // done
+    this.complete();
+  }
+
+  String get primaryKey => this._primaryKey;
+
+  Task get task {
+    assert(this.state == JobState.completed);
+    return this._task;
+  }
+}
--- pkg/remote/lib/jobs/jobs.dart
+++ pkg/remote/lib/jobs/jobs.dart
@@ -1,3 +1,4 @@
 export 'queue.dart';
 export 'initial_fetch.dart';
 export 'create_task.dart';
+export 'fetch_task.dart';

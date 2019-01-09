69a004854a40b25afb9c80bfc6d5b1d3f460b29f
Alexandru Barbur <root.ctrlc@gmail.com>
2019-01-09 17:47:44 -0500

    Add initial widgets for controls to the control set card.

--- pkg/remote-mobile/lib/widgets/control_set_card.dart
+++ pkg/remote-mobile/lib/widgets/control_set_card.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:remote/repository.dart';
 import 'package:remote/stores/control_set.dart';
+import 'package:remote/models/models.dart';
 
 class ControlSetCard extends StatefulWidget {
   final Repository repository;
@@ -17,6 +18,10 @@ class _ControlSetCardState extends State<ControlSetCard> {
 
   void onControlSetChanged() {
     this.setState(() {});
+  }
+
+  void onButtonControlClick() {
+    // TODO
   }
 
   @override
@@ -47,35 +52,82 @@ class _ControlSetCardState extends State<ControlSetCard> {
   @override
   Widget build(BuildContext context) {
     final controlSet = this._notifier.value;
+    final headerRow = Row(
+      children: [
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                controlSet.name,
+                style: DefaultTextStyle.of(context).style.apply(fontSizeFactor: 1.25),
+              ),
+              Text(
+                controlSet.description,
+                style: DefaultTextStyle.of(context).style.copyWith(fontWeight: FontWeight.w300),
+              ),
+            ],
+          ),
+        ),
+        IconButton(
+          icon: Icon(Icons.details),
+          color: Color.fromRGBO(0, 196, 0, 1.0),
+          tooltip: "Details",
+          onPressed: () {},
+        ),
+      ],
+    );
+
+    // XXX: should the repository or store help here?
+    final controls = this.widget.repository.controls.values
+      .where((Control control) => control.controlSetUuid == controlSet.uuid)
+      .toList();
+
+    // XXX: sort order?
+    // controls.sort((Control a, Control b) => a.name.compareTo(b.name));
+
+    final controlRows = controls.map((Control control) {
+      switch (control.type) {
+        case ControlType.button:
+          final buttonControl = control as ButtonControl;
+          return Row(
+            children: [
+              Expanded(
+                child: RaisedButton(
+                  onPressed: this.onButtonControlClick,
+                  child: Text(buttonControl.title)
+                )
+              )
+            ]
+          );
+
+        case ControlType.color:
+          // TODO: implement a color picker widget
+
+        case ControlType.none:
+          return Row(
+            children: [
+              Expanded(
+                child: Text(control.description)
+              )
+            ],
+          );
+      }
+    });
+
+    // XXX: this feels dirty
+    List<Widget> widgets = List<Widget>();
+    widgets.add(headerRow);
+    widgets.addAll(controlRows);
+
     return Card(
+      margin: const EdgeInsets.all(8.0),
       child: Container(
         padding: const EdgeInsets.all(8.0),
-        child: Row(
-          children: [
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    controlSet.name,
-                    style: DefaultTextStyle.of(context).style.apply(fontSizeFactor: 1.25),
-                  ),
-                  Text(
-                    controlSet.description,
-                    style: DefaultTextStyle.of(context).style.copyWith(fontWeight: FontWeight.w300),
-                  ),
-                ],
-              ),
-            ),
-            IconButton(
-              icon: Icon(Icons.details),
-              color: Color.fromRGBO(0, 196, 0, 1.0),
-              tooltip: "Details",
-              onPressed: () {},
-            ),
-          ],
-        ),
-      ),
+        child: Column(
+          children: widgets,
+        )
+      )
     );
   }
 }

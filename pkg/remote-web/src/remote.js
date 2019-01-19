dc0380f96b3195c773a5e3cc5f61d3172dbee851
Alexandru Barbur <root.ctrlc@gmail.com>
2019-01-18 22:44:17 -0500

    All views use templates and unique IDs.

--- pkg/remote-web/src/remote.js
+++ pkg/remote-web/src/remote.js
@@ -50,14 +50,19 @@ $(function(){
     tagName: 'tr',
     template: _.template($('#remote-cs-row-tmpl').html()),
     events: {
-      'click button.controlset-delete': 'onDelete'
+      'click button[data-action=delete]': 'onDelete'
     },
     initialize: function() {
+      this.vid = _.uniqueId();
       this.listenTo(this.model, 'change', this.render);
       this.listenTo(this.model, 'destroy', this.remove);
     },
     render: function() {
-      this.$el.html(this.template(this.model.attributes));
+      this.$el.html(this.template({
+        'vid': this.vid,
+        'model': this.model.attributes
+      }));
+
       return this;
     },
     onDelete: function() {
@@ -70,11 +75,12 @@ $(function(){
   var ControlSetTableView = Backbone.View.extend({
     template: _.template($('#remote-cs-table-tmpl').html()),
     initialize: function() {
+      this.vid = _.uniqueId();
       this.listenTo(this.collection, 'update sync', this.render);
       this.collection.fetch();
     },
     render: function() {
-      this.$el.html(this.template());
+      this.$el.html(this.template({'vid': this.vid}));
       var $tbody = this.$('tbody');
 
       $tbody.empty();
@@ -87,29 +93,35 @@ $(function(){
     }
   });
 
-  // Application view
+  // Application views
   // ********************************
-  var RemoteView = Backbone.View.extend({
-    el: $("#remote"),
+  var DashboardView = Backbone.View.extend({
+    template: _.template($('#remote-dashboard-tmpl').html()),
     events: {
-      'click #controlset-create': 'onCreate'
+      'click input[data-action=create]': 'onCreate'
     },
     initialize: function() {
-      // control sets
       this.controlSets = new ControlSetCollection();
-      this.controlSetsList = new ControlSetTableView({
-        el: this.$('.remote-controlsets'),
-        collection: this.controlSets,
+      this.vid = _.uniqueId();
+      this.render();
+    },
+    render: function() {
+      this.$el.html(this.template({'vid': this.vid}));
+      new ControlSetTableView({
+        el: this.$(`#${this.vid}-controlsets`),
+        collection: this.controlSets
       });
+
+      return this;
     },
     onCreate: function(event) {
       // don't submit the form
       event.preventDefault();
 
       // retrieve form values
-      var $name = this.$('#controlset-name');
-      var $configType = this.$('#controlset-configType');
-      var $description = this.$('#controlset-description');
+      var $name = this.$(`#${this.vid}-name`);
+      var $configType = this.$(`#${this.vid}-configType`);
+      var $description = this.$(`#${this.vid}-description`);
 
       // TODO: validation
 
@@ -121,13 +133,41 @@ $(function(){
       });
 
       // reset the form
-      this.$('#controlset-reset').click();
+      this.$('input[data-action=reset]').click();
+    }
+  });
+
+  // Application router
+  // ********************************
+  var RemoteRouter = Backbone.Router.extend({
+    routes: {
+      'dashboard':        'dashboard',
+      'controlset/:uuid': 'controlSet'
+    },
+    activeView: null,
+    dashboard: function() {
+      if (this.activeView) {
+        this.activeView.remove();
+      }
+
+      this.activeView = new DashboardView({
+        el: $('#remote')
+      });
+    },
+    controlSet: function(uuid) {
+      if (this.activeView) {
+        this.activeView.remove();
+      }
+
+      this.activeView = null;
     }
   });
 
   // Initialize application
   // ********************************
-  var app = new RemoteView();
+  var router = new RemoteRouter();
+  Backbone.history.start();
+  router.navigate("dashboard", {trigger: true});
 
   // DEBUG
   // ********************************
